4651e4750616a091cee8f73dae7fca4c8f72befb
Artem <goldtree3000@gmail.com>
2024-03-20 19:15:59 +0300

    solve logger task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -181,8 +181,15 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argString = args.map((el) => JSON.stringify(el)).join(',');
+    const funcName = func.name;
+    logFunc(`${funcName}(${argString}) starts`);
+    const result = func(...args);
+    logFunc(`${funcName}(${argString}) ends`);
+    return result;
+  };
 }
 
 /**
